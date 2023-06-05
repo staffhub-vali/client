4a2233237b2d57173e484f10c5a8174939182646
valyy151 <marin.valenta1@gmail.com>
2023-06-05 06:42:54 +0200

    Creation of notes for workDays

--- src/components/WorkDay/WorkDay.tsx
+++ src/components/WorkDay/WorkDay.tsx
@@ -1,8 +1,10 @@
+import axios from 'axios'
 import Employee from './Employee'
 import Button from '../ui/Button'
 import AddShift from './AddShift'
 import Heading from '../ui/Heading'
 import { FC, useState } from 'react'
+import Paragraph from '../ui/Paragraph'
 import Notification from '../ui/Notification'
 import { formatDate } from '../../utils/DateFormatting'
 
@@ -31,7 +33,43 @@ interface WorkDay {
 }
 
 const WorkDay: FC<WorkDayProps> = ({ workDay, setWorkDay, error, setError, message, setMessage }) => {
-	const [showAddShift, setShowAddShift] = useState(false)
+	const [note, setNote] = useState<string>('')
+	const [isLoading, setIsLoading] = useState<boolean>(false)
+	const [showAddNote, setShowAddNote] = useState<boolean>(false)
+	const [showAddShift, setShowAddShift] = useState<boolean>(false)
+
+	const addNote = async () => {
+		if (!showAddNote) {
+			setShowAddNote(true)
+			setShowAddShift(false)
+			return
+		}
+		setIsLoading(true)
+		try {
+			const token = localStorage.getItem('token')
+			const { data } = await axios.post(
+				'http://localhost:8080/v1/days/notes',
+				{
+					day: workDay?._id,
+					note: note,
+				},
+				{
+					headers: {
+						Authorization: `Bearer ${token}`,
+					},
+				},
+			)
+			setNote('')
+			setError('')
+			setIsLoading(false)
+			setShowAddNote(false)
+			setMessage(data.message)
+		} catch (error: any) {
+			setMessage('')
+			setIsLoading(false)
+			setError(error.response.data.message)
+		}
+	}
 
 	return (
 		<>
@@ -45,7 +83,7 @@ const WorkDay: FC<WorkDayProps> = ({ workDay, setWorkDay, error, setError, messa
 				<Heading size={'xs'}> There are currently no shifts for this day. </Heading>
 			)}
 
-			<div className='flex flex-col items-center space-y-2 border-b-2 py-12 dark:border-slate-700'>
+			<div className='flex flex-col items-center space-y-2 border-b-2 py-6 dark:border-slate-700'>
 				{workDay?.shifts?.map((shift, index) => (
 					<Employee
 						shift={shift}
@@ -56,6 +94,42 @@ const WorkDay: FC<WorkDayProps> = ({ workDay, setWorkDay, error, setError, messa
 						setWorkDay={setWorkDay}
 					/>
 				))}
+			</div>
+			<div className='flex w-10/12 flex-col items-center space-y-6 border-b-2 py-6 dark:border-slate-700'>
+				{workDay && workDay.notes.length > 0 && <Heading size={'sm'}>Notes</Heading>}
+				{workDay &&
+					workDay.notes.length > 0 &&
+					workDay.notes.map((note) => (
+						<Paragraph
+							size={'sm'}
+							key={workDay._id}
+							className='mb-4'>
+							{note}
+						</Paragraph>
+					))}{' '}
+				{!showAddNote && workDay && workDay.notes.length < 1 && (
+					<Paragraph
+						className='py-12'
+						size={'xl'}>
+						There are no notes for this day.
+					</Paragraph>
+				)}
+				{showAddNote && (
+					<textarea
+						name='note'
+						rows={5}
+						cols={50}
+						className='resize-none rounded p-2 shadow'
+						placeholder='Anything to note?'
+						onChange={(e) => setNote(e.target.value)}>
+						{note}
+					</textarea>
+				)}
+				<Button
+					onClick={addNote}
+					className=''>
+					Add Note
+				</Button>
 			</div>
 
 			{showAddShift ? (
@@ -69,7 +143,10 @@ const WorkDay: FC<WorkDayProps> = ({ workDay, setWorkDay, error, setError, messa
 				<Button
 					size={'lg'}
 					className='mt-12'
-					onClick={() => setShowAddShift(true)}>
+					onClick={() => {
+						setShowAddShift(true)
+						setShowAddNote(false)
+					}}>
 					New Shift
 				</Button>
 			)}

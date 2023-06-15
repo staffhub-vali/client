ea44e17f238f8273974815572df34dd80977dba6
valyy151 <marin.valenta1@gmail.com>
2023-06-15 10:18:02 +0200

    Added schedule saving as pdf

--- src/components/Employee/Edit/Schedule/Schedule.tsx
+++ src/components/Employee/Edit/Schedule/Schedule.tsx
@@ -1,11 +1,13 @@
 import { Calendar } from 'react-calendar'
+import Button from '../../../ui/Button.tsx'
 import Heading from '../../../ui/Heading.tsx'
 import Container from '../../../ui/Container.tsx'
 import Paragraph from '../../../ui/Paragraph.tsx'
 import { Link, useNavigate } from 'react-router-dom'
 import { Dispatch, FC, SetStateAction, useState } from 'react'
+import ReactPDF, { Document, Page, Text, View, StyleSheet, PDFDownloadLink } from '@react-pdf/renderer'
 import { calculateMonthlyHours, calculateTotalHours } from '../../../../utils/CalculateHours.ts'
-import { formatDate, formatDay, formatMonth, formatTime } from '../../../../utils/DateFormatting.ts'
+import { formatDate, formatDay, formatMonth, formatTime, formatTotal } from '../../../../utils/DateFormatting.ts'
 
 interface ScheduleProps {
 	employee: {
@@ -32,6 +34,33 @@ interface Shift {
 	workDay: { date: number; _id: string }
 }
 
+const styles = StyleSheet.create({
+	page: {
+		flexDirection: 'column',
+		backgroundColor: 'white',
+	},
+	title: {
+		textAlign: 'center',
+		fontSize: 24,
+		padding: 16,
+		borderBottom: '1px solid black',
+	},
+	section: {
+		padding: 3.32,
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-evenly',
+		alignItems: 'center',
+		borderBottom: '1px solid #ccc',
+	},
+
+	shift: {
+		width: 200,
+		fontSize: 16,
+		textAlign: 'center',
+	},
+})
+
 const Schedule: FC<ScheduleProps> = ({ loading, setError, setMessage, employee, setLoading, shifts }) => {
 	const navigate = useNavigate()
 
@@ -50,8 +79,32 @@ const Schedule: FC<ScheduleProps> = ({ loading, setError, setMessage, employee, 
 			return startMonth === date.getMonth()
 		})
 
-		setFilteredShifts(filteredShifts)
+		setFilteredShifts(filteredShifts.sort((a, b) => a.workDay.date - b.workDay.date))
 	}
+
+	const MyDocument = () => (
+		<Document pageLayout='singlePage'>
+			<Page
+				size='A4'
+				orientation='portrait'
+				style={styles.page}>
+				<View style={styles.title}>
+					<Text>
+						{employee.name} - {month} ({calculateTotalHours(filteredShifts)}h)
+					</Text>
+				</View>
+				{filteredShifts.map((shift) => (
+					<View style={styles.section}>
+						<Text style={styles.shift}>{formatDate(shift.workDay.date)}</Text>
+						<Text style={styles.shift}>
+							{formatTime(shift.start)} - {formatTime(shift.end)}
+						</Text>
+						<Text style={styles.shift}>{formatTotal(shift.start, shift.end)}</Text>
+					</View>
+				))}
+			</Page>
+		</Document>
+	)
 
 	return (
 		<Container size={'lg'}>
@@ -59,14 +112,16 @@ const Schedule: FC<ScheduleProps> = ({ loading, setError, setMessage, employee, 
 
 			<Heading
 				size={'xs'}
-				className='my-3 w-full border-b-2 p-4 text-center font-normal'>
+				className='my-3 w-full border-b-2 p-4 text-center font-normal dark:border-slate-700'>
 				Total hours for {formatMonth(new Date().getTime() / 1000)} - {calculateMonthlyHours(shifts)}
 			</Heading>
 
 			<div className='mt-4 flex w-full'>
 				<div
 					className={`${
-						filteredShifts.length > 0 ? 'overflow-y-scroll border bg-white shadow' : 'border-none'
+						filteredShifts.length > 0
+							? 'overflow-y-scroll border bg-white shadow dark:border-slate-500 dark:bg-slate-800'
+							: 'border-none'
 					}  mx-auto h-[37rem] overflow-x-hidden rounded border`}>
 					{filteredShifts.length <= 0 && (
 						<Heading
@@ -77,16 +132,26 @@ const Schedule: FC<ScheduleProps> = ({ loading, setError, setMessage, employee, 
 					)}
 					{filteredShifts.length > 0 && month && (
 						<Heading
-							size={'sm'}
-							className='border-b-2 bg-white pb-3 pt-6 text-center font-normal'>
+							size={'xs'}
+							className='text-md flex items-center justify-evenly border-b-2 border-t bg-white py-4 text-center font-normal dark:border-slate-500 dark:bg-slate-800'>
 							{formatMonth(value.getTime() / 1000)} - {filteredShifts.length}{' '}
 							{filteredShifts.length === 1 ? 'Shift' : 'Shifts'} ({calculateTotalHours(filteredShifts)} hours)
+							<Button
+								size={'lg'}
+								className='text-xl hover:text-sky-500 dark:hover:text-sky-400'
+								variant={'outlineHover'}>
+								<PDFDownloadLink
+									document={<MyDocument />}
+									fileName={`${employee.name} - ${formatMonth(value.getTime() / 1000)}`}>
+									Save as PDF
+								</PDFDownloadLink>
+							</Button>
 						</Heading>
 					)}
 					{!month && filteredShifts.length > 0 && (
 						<Heading
 							size={'sm'}
-							className='border-b-2 bg-white pb-3 pt-6 text-center font-normal'>
+							className='border-b-2 bg-white pb-3 pt-6 text-center font-normal dark:border-slate-500 dark:bg-slate-800'>
 							All shifts
 						</Heading>
 					)}
@@ -95,13 +160,13 @@ const Schedule: FC<ScheduleProps> = ({ loading, setError, setMessage, employee, 
 							<div
 								key={shift._id}
 								onClick={() => navigate(`/days/${shift.workDay._id}`)}
-								className={`group flex w-[48rem] cursor-pointer items-center space-y-4 border-b-2 ${
-									index % 2 === 0 ? 'bg-slate-50' : 'bg-white'
+								className={`group flex w-[48rem] cursor-pointer items-center space-y-4 border-b-2 dark:border-slate-500 ${
+									index % 2 === 0 ? 'bg-slate-50 dark:bg-slate-700' : 'bg-white dark:bg-slate-800'
 								} py-2`}>
-								<div className='mx-auto flex flex-col items-center group-hover:text-sky-500'>
+								<div className='mx-auto flex flex-col items-center group-hover:text-sky-500 dark:group-hover:text-sky-400'>
 									{formatDay(shift.workDay.date)}
 									<Paragraph
-										className='group-hover:text-sky-500'
+										className='group-hover:text-sky-500 dark:group-hover:text-sky-400'
 										size={'xl'}>
 										{formatDate(shift.workDay.date)}
 									</Paragraph>
@@ -109,7 +174,7 @@ const Schedule: FC<ScheduleProps> = ({ loading, setError, setMessage, employee, 
 
 								<Paragraph
 									size={'xl'}
-									className='mx-auto pb-2 group-hover:text-sky-500'>
+									className='mx-auto pb-2 group-hover:text-sky-500 dark:group-hover:text-sky-400'>
 									{formatTime(shift.start)} - {formatTime(shift.end)}
 								</Paragraph>
 							</div>
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -39,70 +39,66 @@ const HomePage = () => {
 	}
 
 	return (
-		<Container size={'lg'}>
+		<Container
+			size={'lg'}
+			className='p-0 px-24 pt-24'>
 			{loading ? (
 				<Spinner />
 			) : (
 				<>
-					<div className='flex'>
-						<a
-							className='hover:text-underline inline-block h-10 p-2 text-center text-blue-300 no-underline hover:text-indigo-800 md:h-auto md:p-4'
-							data-tippy-content='@twitter_handle'
-							href='https://twitter.com/intent/tweet?url=#'>
-							<svg
-								className='h-6 fill-current'
-								xmlns='http://www.w3.org/2000/svg'
-								viewBox='0 0 32 32'>
-								<path d='M30.063 7.313c-.813 1.125-1.75 2.125-2.875 2.938v.75c0 1.563-.188 3.125-.688 4.625a15.088 15.088 0 0 1-2.063 4.438c-.875 1.438-2 2.688-3.25 3.813a15.015 15.015 0 0 1-4.625 2.563c-1.813.688-3.75 1-5.75 1-3.25 0-6.188-.875-8.875-2.625.438.063.875.125 1.375.125 2.688 0 5.063-.875 7.188-2.5-1.25 0-2.375-.375-3.375-1.125s-1.688-1.688-2.063-2.875c.438.063.813.125 1.125.125.5 0 1-.063 1.5-.25-1.313-.25-2.438-.938-3.313-1.938a5.673 5.673 0 0 1-1.313-3.688v-.063c.813.438 1.688.688 2.625.688a5.228 5.228 0 0 1-1.875-2c-.5-.875-.688-1.813-.688-2.75 0-1.063.25-2.063.75-2.938 1.438 1.75 3.188 3.188 5.25 4.25s4.313 1.688 6.688 1.813a5.579 5.579 0 0 1 1.5-5.438c1.125-1.125 2.5-1.688 4.125-1.688s3.063.625 4.188 1.813a11.48 11.48 0 0 0 3.688-1.375c-.438 1.375-1.313 2.438-2.563 3.188 1.125-.125 2.188-.438 3.313-.875z'></path>
-							</svg>
-						</a>
-						<a
-							className='hover:text-underline inline-block h-10 p-2 text-center text-blue-300 no-underline hover:text-indigo-800 md:h-auto md:p-4 '
-							data-tippy-content='#facebook_id'
-							href='https://www.facebook.com/sharer/sharer.php?u=#'>
-							<svg
-								className='h-6 fill-current'
-								xmlns='http://www.w3.org/2000/svg'
-								viewBox='0 0 32 32'>
-								<path d='M19 6h5V0h-5c-3.86 0-7 3.14-7 7v3H8v6h4v16h6V16h5l1-6h-6V7c0-.542.458-1 1-1z'></path>
-							</svg>
-						</a>
-					</div>
-
-					<div className='flex w-full pt-12'>
-						<div className='flex flex-col justify-center overflow-y-hidden'>
-							<h1 className='slide-in-bottom-h1 text-center text-3xl font-bold leading-tight text-purple-800 md:text-left md:text-5xl'>
+					<div className='flex w-full items-center pt-12'>
+						<div className='ml-8 mr-auto flex flex-col items-center overflow-y-hidden'>
+							<h1 className='slide-in-bottom-h1 text-center text-3xl font-bold leading-tight text-sky-600 md:text-left md:text-5xl'>
 								Main Hero Message to sell your app
 							</h1>
-							<p className='slide-in-bottom-subtitle text-center text-base leading-normal md:text-left md:text-2xl'>
+							<p className='slide-in-bottom-subtitle pt-2 text-center text-base leading-normal md:text-left md:text-2xl'>
 								Sub-hero message, not too long and not too short. Make it just right!
 							</p>
 
-							<p className='fade-in text-center font-bold text-blue-400 md:text-left'>Download our app:</p>
-							<div className='fade-in flex w-full md:justify-start'>
-								<img
-									src='./app-store.svg'
-									className='bounce-top-icons h-12'
-								/>
-								<img
-									src='./play-store.svg'
-									className='bounce-top-icons h-12'
-								/>
+							<div className='fade-in flex w-full justify-center space-x-2 pt-6'>
+								<Button className='bounce-top-icons'>Get started</Button>
+								<Button
+									className='bounce-top-icons'
+									variant={'outline'}>
+									How to use
+								</Button>
 							</div>
 						</div>
 
-						<div className='ml-auto w-1/2 overflow-y-hidden'>
-							<img
-								className='slide-in-bottom mx-auto lg:mr-0'
-								src='./date-icon.svg'
-							/>
-						</div>
+						<img
+							className='slide-in-bottom mx-auto lg:mr-0'
+							src='./calendar.svg'
+						/>
 					</div>
-					<div className='fade-in w-full text-center text-sm md:text-center'>
+					<div className='fade-in flex w-full flex-col items-center pt-80 text-center text-sm md:text-center'>
+						<div className='flex '>
+							<a
+								className='hover:text-underline inline-block h-10 p-2 text-center text-blue-300 no-underline hover:text-indigo-800 md:h-auto md:p-4'
+								data-tippy-content='@twitter_handle'
+								href='https://twitter.com/intent/tweet?url=#'>
+								<svg
+									className='h-6 fill-current'
+									xmlns='http://www.w3.org/2000/svg'
+									viewBox='0 0 32 32'>
+									<path d='M30.063 7.313c-.813 1.125-1.75 2.125-2.875 2.938v.75c0 1.563-.188 3.125-.688 4.625a15.088 15.088 0 0 1-2.063 4.438c-.875 1.438-2 2.688-3.25 3.813a15.015 15.015 0 0 1-4.625 2.563c-1.813.688-3.75 1-5.75 1-3.25 0-6.188-.875-8.875-2.625.438.063.875.125 1.375.125 2.688 0 5.063-.875 7.188-2.5-1.25 0-2.375-.375-3.375-1.125s-1.688-1.688-2.063-2.875c.438.063.813.125 1.125.125.5 0 1-.063 1.5-.25-1.313-.25-2.438-.938-3.313-1.938a5.673 5.673 0 0 1-1.313-3.688v-.063c.813.438 1.688.688 2.625.688a5.228 5.228 0 0 1-1.875-2c-.5-.875-.688-1.813-.688-2.75 0-1.063.25-2.063.75-2.938 1.438 1.75 3.188 3.188 5.25 4.25s4.313 1.688 6.688 1.813a5.579 5.579 0 0 1 1.5-5.438c1.125-1.125 2.5-1.688 4.125-1.688s3.063.625 4.188 1.813a11.48 11.48 0 0 0 3.688-1.375c-.438 1.375-1.313 2.438-2.563 3.188 1.125-.125 2.188-.438 3.313-.875z'></path>
+								</svg>
+							</a>
+							<a
+								className='hover:text-underline inline-block h-10 p-2 text-center text-blue-300 no-underline hover:text-indigo-800 md:h-auto md:p-4 '
+								data-tippy-content='#facebook_id'
+								href='https://www.facebook.com/sharer/sharer.php?u=#'>
+								<svg
+									className='h-6 fill-current'
+									xmlns='http://www.w3.org/2000/svg'
+									viewBox='0 0 32 32'>
+									<path d='M19 6h5V0h-5c-3.86 0-7 3.14-7 7v3H8v6h4v16h6V16h5l1-6h-6V7c0-.542.458-1 1-1z'></path>
+								</svg>
+							</a>
+						</div>
 						<a
 							className='text-gray-500 no-underline hover:no-underline'
 							href='#'>
-							&copy; App 2019
+							&copy; App 2019 Marin Valenta
 						</a>
 					</div>
 				</>
